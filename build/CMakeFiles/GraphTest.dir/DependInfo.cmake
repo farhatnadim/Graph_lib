a3a35fe19498a8894f569c01512d321d5e1e4b8b
farhatnadim <nadim.farhat@gmail.com>
2024-08-02 08:17:38 -0400

    Added Google Test, Moved Graph_Test to test

---
+++ build/CMakeFiles/GraphTest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/source/Graph_lib/Graph.cpp" "CMakeFiles/GraphTest.dir/Graph.cpp.o" "gcc" "CMakeFiles/GraphTest.dir/Graph.cpp.o.d"
+  "/mnt/d/source/Graph_lib/Symbol.cpp" "CMakeFiles/GraphTest.dir/Symbol.cpp.o" "gcc" "CMakeFiles/GraphTest.dir/Symbol.cpp.o.d"
+  "/mnt/d/source/Graph_lib/graph_test_program.cpp" "CMakeFiles/GraphTest.dir/graph_test_program.cpp.o" "gcc" "CMakeFiles/GraphTest.dir/graph_test_program.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/external/googletest-main/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/source/Graph_lib/external/googletest-main
+# Build directory: /mnt/d/source/Graph_lib/build/external/googletest-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
