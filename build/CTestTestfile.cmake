db33bbd67e28c31fcd1a16d2d8ddb36570bd6c43
Nadim Farhat <nfarhat@iotamotion.com>
2024-08-02 20:59:26 -0400

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/source/Graph_lib
-# Build directory: /mnt/d/source/Graph_lib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
