4d1cdfd3546d3995c23425483f7f3699d07cab14
farhatnadim <nadim.farhat@gmail.com>
2024-08-07 22:16:11 -0400

    added GraphAlgo

--- Graph.hpp
+++ Graph.hpp
@@ -37,7 +37,8 @@ public:
                     addEdge(u, v);
                 }
             }
-        }
+         }
+        
     }
 
 
---
+++ GraphAlgo.hpp
@@ -0,0 +1,15 @@
+#pragma once 
+// According to sedgwick Page 557 Q&A: and I paraphrase, the graph algorithms are
+// too numerous to be included as a part of a class therefore i am going to write all the
+// algo here. Naiively i started by implementing them as methods but now i see that i shouldn't
+
+#include "Graph.hpp"
+
+template <typename T>
+bool cycle<T> (Graph<T> G) 
+{   
+    bool hasCycle = false;
+
+
+    return hasCycle;
+}
